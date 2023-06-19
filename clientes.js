be25698d15bd5718260376934727fce7cf7c04ea
jeliton soares <jelitonn.soarres@gmail.com>
2023-06-19 16:08:39 -0300

    Criando uma lista de clientes ficticia

---
+++ clientes.js
@@ -0,0 +1,51 @@
+const homens = [
+    {
+        nome: 'Jeliton',
+        idade: 21,
+    },
+    {
+        nome: 'Vitor',
+        idade: 22,
+    },
+    {
+        nome: 'Elias',
+        idade: 58,
+    },
+    {
+        nome: 'Valerio',
+        idade: 59,
+    },
+    {
+        nome: 'Miguel',
+        idade: 62,
+    }
+]
+
+const mulheres = [
+    {
+        nome: 'Jerlane',
+        idade: 20,
+    },
+    {
+        nome: 'Julia',
+        idade: 22,
+    },
+    {
+        nome: 'Elisa',
+        idade: 26,
+    },
+    {
+        nome: 'Mazinha',
+        idade: 44,
+    },
+    {
+        nome: 'Terezinha',
+        idade: 52,
+    },
+    {
+        nome: 'Celia',
+        idade: 59,
+    }
+]
+
+module.exports = { homens, mulheres };
